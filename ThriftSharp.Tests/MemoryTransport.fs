c848bcbff67c1a334747ffd0a129e3ffe73e6875
Solal Pirelli <devnull@localhost>
2013-09-11 00:07:29 +0200

    Added writing tests for the binary protocol.

---
+++ ThriftSharp.Tests/MemoryTransport.fs
@@ -0,0 +1,38 @@
+﻿namespace ThriftSharp.Tests
+
+open System.Collections.Generic
+open ThriftSharp.Transport
+
+type MemoryTransport(toRead: byte list) =
+    let mutable writtenVals = []
+    let mutable hasRead = false
+    let toRead = Queue(toRead)
+
+    let write values = for value in values do writtenVals <- value::writtenVals
+    let read len = [| for x = 1 to len do
+                          if toRead.Count > 0 then yield toRead.Dequeue()
+                          else failwith "Not enough bytes were read." |]
+
+    member x.WrittenValues with get() = List.rev writtenVals
+    member x.IsEmpty with get() = toRead.Count = 0
+
+    new() = MemoryTransport([])
+
+    interface IThriftTransport with
+        member x.WriteByte(b) =
+            (x :> IThriftTransport).WriteBytes([| b |])
+
+        member x.WriteBytes(bs) =
+            if hasRead then failwith "Cannot write after a read. Close the transport first."
+            write bs
+
+        member x.ReadByte() =
+            (x :> IThriftTransport).ReadBytes(1).[0]
+            
+        member x.ReadBytes(len) =
+            if not hasRead then
+                hasRead <- true
+            read(len)
+
+        member x.Close() =
+            hasRead <- false
