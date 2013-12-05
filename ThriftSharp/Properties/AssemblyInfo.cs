d8d2c7611fcf1a9c09c9c4b9ef2391c26be04272
Solal Pirelli <devnull@localhost>
2013-12-05 12:57:01 +0100

    Incremented version number to 1.0.5.

--- ThriftSharp/Properties/AssemblyInfo.cs
+++ ThriftSharp/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright( "Copyright © Solal Pirelli 2013" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
-[assembly: AssemblyVersion( "1.0.4.*" )]
+[assembly: AssemblyVersion( "1.0.5.*" )]
 
 [assembly: InternalsVisibleTo( "ThriftSharp.Extensions" )]
 [assembly: InternalsVisibleTo( "ThriftSharp.Tests" )]
