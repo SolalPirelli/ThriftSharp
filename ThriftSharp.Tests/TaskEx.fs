7a7e8273985141bfe9b12770eca00bc56271216b
Solal Pirelli <solal.pirelli@gmail.com>
2016-01-10 22:50:32 +0100

    Change some timeouts in TaskEx tests.
    Try to fix CI failures.

--- ThriftSharp.Tests/TaskEx.fs
+++ ThriftSharp.Tests/TaskEx.fs
@@ -6,7 +6,7 @@ open System.Threading.Tasks
 open Xunit
 open ThriftSharp.Utilities
 
-let MaxTimeout = TimeSpan.FromMilliseconds(40.0)
+let MaxTimeout = TimeSpan.FromMilliseconds(100.0)
 
 [<Fact>]
 let ``Already-completed task``() = asTask <| async {
@@ -30,7 +30,7 @@ let ``Already-canceled task``() = asTask <| async {
 [<Fact>]
 let ``Quick task``() = asTask <| async {
     let source = TaskCompletionSource<int>()
-    let task = Task.Delay(10).ContinueWith(fun _ -> source.TrySetResult(42))
+    let task = Task.Delay(5).ContinueWith(fun _ -> source.TrySetResult(42))
     let! result = TaskEx.TimeoutAfter(source.Task, MaxTimeout) |> Async.AwaitTask
     result <=> 42
     do! task |> Async.AwaitTask |> Async.Ignore
@@ -39,7 +39,7 @@ let ``Quick task``() = asTask <| async {
 [<Fact>]
 let ``Quickly faulted task``() = asTask <| async {
     let source = TaskCompletionSource<int>()
-    let task = Task.Delay(10).ContinueWith(fun _ -> source.TrySetException(Exception("Oops")))
+    let task = Task.Delay(5).ContinueWith(fun _ -> source.TrySetException(Exception("Oops")))
     let! exn = Assert.ThrowsAsync<Exception>(fun () -> TaskEx.TimeoutAfter(source.Task, MaxTimeout) :> Task) |> Async.AwaitTask
     exn.Message <=> "Oops"
     do! task |> Async.AwaitTask |> Async.Ignore
@@ -48,7 +48,7 @@ let ``Quickly faulted task``() = asTask <| async {
 [<Fact>]
 let ``Quickly canceled task``() = asTask <| async {
     let source = TaskCompletionSource<int>()
-    let task = Task.Delay(10).ContinueWith(fun _ -> source.TrySetCanceled(CancellationToken(false)))
+    let task = Task.Delay(5).ContinueWith(fun _ -> source.TrySetCanceled(CancellationToken(false)))
     do! Assert.ThrowsAnyAsync<OperationCanceledException>(fun () ->
             TaskEx.TimeoutAfter(source.Task, MaxTimeout) :> Task) |> Async.AwaitTask |> Async.Ignore
     do! task |> Async.AwaitTask |> Async.Ignore
@@ -64,7 +64,7 @@ let ``Never-completing task``() = asTask <| async {
 [<Fact>]
 let ``Very lengthy task``() = asTask <| async {
     let source = TaskCompletionSource<int>()
-    Task.Delay(100).ContinueWith(fun _ -> source.TrySetResult(0)) |> ignore
+    Task.Delay(500).ContinueWith(fun _ -> source.TrySetResult(0)) |> ignore
     do! Assert.ThrowsAnyAsync<OperationCanceledException>(fun () ->
             TaskEx.TimeoutAfter(source.Task, MaxTimeout) :> Task) |> Async.AwaitTask |> Async.Ignore
 }
