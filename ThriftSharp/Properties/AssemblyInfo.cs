e9c5d9cdfa35d856cc543b771744c2f71c508724
Solal Pirelli <solal.pirelli@gmail.com>
2016-09-24 14:45:31 +0200

    Add rd.xml file to the package output.

--- ThriftSharp/Properties/AssemblyInfo.cs
+++ ThriftSharp/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 // This code is licensed under the MIT License (see Licence.txt for details)
 
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: ComVisible( false )]
 [assembly: InternalsVisibleTo( "ThriftSharp.Benchmarking" )]
-[assembly: InternalsVisibleTo( "ThriftSharp.Extensions" )]
 [assembly: InternalsVisibleTo( "ThriftSharp.Tests" )]
